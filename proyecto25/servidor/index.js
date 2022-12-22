196ba63794d421caf6d45769b2458caa62b19634
Paula Sosa <paulasosa0719@outlook.com>
2022-12-22 13:39:01 -0500

    Soporte clases Sprint 4

---
+++ proyecto25/servidor/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const conectarDB = require("./config/db");
+const cors = require("cors");
+
+//crear el servidor
+const app = express();
+
+//conectar a la base de datos
+conectarDB();
+
+//habilitar cors
+app.use(cors());
+
+//Habilite express.json
+app.use(express.json({ extended: true }));
+
+const PORT = process.env.PORT || 4000;
+
+//importar rutas
+app.use("/api/usuarios", require("./routes/usuarios"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/proyectos", require("./routes/proyectos"));
+app.use("/api/tareas", require("./routes/tareas"));
+
+// arrancar la app
+app.listen(PORT, () => {
+  console.log(`El servidor está funcionando en el puerto ${PORT} `);
+});
